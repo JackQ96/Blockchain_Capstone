5a471066aca8b84796af053c5814c21c65ea52ae
Jack Quinn <jackquinn2@msn.com>
2022-09-28 17:47:56 +0100

    all files compiling

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,28 +1,78 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+import "./ERC721Mintable.sol";
+
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+
+contract Verifier {
+    function verifyTx(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[2] memory inputs
+    )
+
+    public
+    returns (bool r);
+}
 
 
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 
+contract SolnSquareVerifier is ERC721Token {
+    Verifier private verifierContract;
+
+    constructor(address verifierAddress, string memory name, string memory symbol)
+    ERC721Token(name, symbol)
+    public
+    {
+        verifierContract = Verifier(verifierAddress);
+    }
 
 
 // TODO define a solutions struct that can hold an index & an address
+    struct Solution {
+        uint256 solutionIndex;
+        address solutionAddress;
+        bool minted;
+    }
 
 
 // TODO define an array of the above struct
+    uint256 public numOfSol = 0;
 
 
 // TODO define a mapping to store unique solutions submitted
+    mapping(bytes32 => Solution) solutions;
 
 
 
 // TODO Create an event to emit when a solution is added
+    event AddSolutions(uint256 solutionIndex, address indexed solutionAddress);
 
 
 
 // TODO Create a function to add the solutions to the array and emit the event
+    function addToArray(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[2] memory inputs
+    )
+    public
+    {
+        bytes32 solHash = keccak256(abi.encodePacked(inputs[0], inputs[1]));
+        require(solutions[solHash].solutionAddress == address(0), "Already exists");
+
+        bool verified = verifierContract.verifyTx(a, b, c, inputs);
+        require(verified, "Verification failed");
+
+        solutions[solHash] = Solution(numOfSol, msg.sender, false);
+
+        emit AddSolutions(numOfSol, msg.sender);
+        numOfSol++;
+    }
 
 
 
@@ -30,30 +80,17 @@ pragma solidity >=0.4.21 <0.6.0;
 //  - make sure the solution is unique (has not been used before)
 //  - make sure you handle metadata as well as tokenSuplly
 
-  
+    function mintNFT(uint a, uint b, address to)
+    public
+    {
+        bytes32 solHash = keccak256(abi.encodePacked(a, b));
 
+        require(solutions[solHash].solutionAddress == msg.sender, "Only owner can mint new NFT's");
+        require(solutions[solHash].minted == false, "This token has already been minted");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        super.mint(to, solutions[solHash].solutionIndex);
+        solutions[solHash].minted = true;
+    }
+}
 
 
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,59 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
 
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+
+const truffleAssert = require('truffle-assertions');
+const expect = require('chai').expect;
+
+const solnSquareCon = artifacts.require("SolnSquareVerifier");
+const verifierCon = artifacts.require("Verifier");
+const jproof = require("./proof.json");
+
+contract('SolnSquareVerifier', acc => {
+    accounts = acc;
+    owner = accounts[0];
+    receiver = accounts[1];
+
+    const name = "PracticeToken";
+    const symbol = "PTOK";
+    
+
+    describe('See if a new solution can be added', function() {
+        before(async () => {
+            const verifier = await verifierCon.new({ from: owner });
+            instance = await solnSquareCon.new(verifier.address, name, symbol, { from: owner });
+            
+        });
+        
+        it( 'Allow to add the new solution', async () => {
+            let res = await instance.addSolutions(jproof.proof.a, jproof.proof.b, jproof.proof.c, jproof.proof.inputs, { from: owner});
+
+            truffleAssert.eventEmitted(res, 'AddSolutions', (ev) => {
+                return expect(ev.solutionAddress).to.equal(accounts[0]);
+            });
+        });       
+    })
+
+
+    describe('See if an ERC721 token can be minted', function() {
+        before(async () => {
+            const verifier = await verifierCon.new({ from: owner });
+            instance = await solnSquareCon.new(verifier.address, name, symbol, { from: owner });
+            
+        });
+        
+        it( 'Allow ERC721 mint', async () => {
+            let res = await instance.mintNFT(6, 1, receiver, { from: owner});
+
+            truffleAssert.eventEmitted(res, 'Transfer', (ev) => {
+                return expect(ev.to).to.equal(receiver) && expect(Number(ev.tokenId)).to.equal(1);
+            });
+
+            let uri = await instance.tokenURI(1, { from: receiver });
+            assert.equal(uri, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/", "URI is incorrect");
+        });
+
+    
+        
+    })
+})
